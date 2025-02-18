1e4559d2b91fb1bf7f98b887225ddb619b7bfaea
Evigenios <e.chontzidis@web.de>
2025-02-18 11:12:14 +0100

    String variablen erstellt

---
+++ variablen.dart
@@ -0,0 +1,11 @@
+void main() {
+  String gdStr = "Guten";
+  String dayStr = "Tag";
+  String worldStr = "Welt";
+  String timeStr = "Zeit";
+  String dd = "!";
+  String c = "Schöne";
+  String morning = "Morgen";
+  String s = "Heute";
+  String spa = " ";
+}
