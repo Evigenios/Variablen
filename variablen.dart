018c2640d470c99a088139260945c84dd984908e
Evigenios <e.chontzidis@web.de>
2025-02-18 13:12:07 +0100

    Aufgeben erstellt

--- variablen.dart
+++ variablen.dart
@@ -7,12 +7,26 @@ void main() {
   String c = "Schöne";
   String morning = "Morgen";
   String s = "Heute";
-  String spa = " ";
+  //String spa = " ";
 
   //int variabeln
   int d = 20;
   int seven = 7;
   int twelve = 12;
   int a = 24;
-  
+
+  //zweite aufgabe b
+  String goodDayWorld = "$gdStr $dayStr $worldStr$dd";
+  print(goodDayWorld);
+  //zweite aufgabe c
+  print(a + seven);
+
+  //zweite aufgabe d
+  print("$c $timeStr$dd");
+
+  //zweite aufgabe e
+  print("$s $morning$dd");
+
+  //zweite aufgabe f
+  print(twelve * twelve + a);
 }
